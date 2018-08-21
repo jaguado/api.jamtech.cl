d15c08c747a75bc7d9b1efcfd34c02ce49ff92b2
Jorge Aguado Merlet <aguado.jorge@principal.com>
2018-08-21 01:13:06 -0300

    add rank on ui

--- wwwroot/js/controllers.js
+++ wwwroot/js/controllers.js
@@ -19,6 +19,7 @@ function StationsCtrl($http, $scope) {
     var stationsUrl = '//jamtechapi.herokuapp.com/v1/CombustibleStations?type=Vehicular&region=13&order=precios.gasolina_95';
     $scope.searchText="";
     $scope.stations=[];
+
     $scope.searchStations = function() {
         return $http.get(stationsUrl).then(function(response){
             var tempStations=response.data;
