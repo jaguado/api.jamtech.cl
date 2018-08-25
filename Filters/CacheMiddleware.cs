698676bc079ace2aff7451e36e092aa08f75fe71
Jorge Aguado Merlet <aguado.jorge@principal.com>
2018-08-25 16:30:39 -0300

    statics inified from mem cache

---
+++ Filters/CacheMiddleware.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JAMTech.Filters
+{
+    public class CacheMiddleware
+    {
+        //The RequestDelegate represents the next middleware in the pipeline.
+        private RequestDelegate _next;
+
+        //Said delegate must be passed in from the previous middleware.
+        public CacheMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        //The Invoke method is called by the previous middleware 
+        //to kick off the current one.
+        public Task Invoke(HttpContext context)
+        {
+            //custom static files middleware with cache
+            if (Startup.cache)
+            {
+                //get file from cache and put into response
+                var filename = Startup.basePath + context.Request.Path.Value.Replace("/", Path.DirectorySeparatorChar.ToString());
+                if (Startup.staticFilesStorage.TryGetValue(filename, out var result))
+                {
+                    //var stream = new MemoryStream(result.Item2);
+                    //var body = new StreamReader(stream).ReadToEnd();
+                    //context.Response.ContentType = result.Item1;
+                    //context.Response.ContentLength = body.Length;
+                    return Task.Factory.StartNew(() => new FileContentResult(result.Item2, result.Item1));
+                } 
+            }
+            //Finally, we call Invoke() on the next middleware in the pipeline.
+            return _next.Invoke(context);
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -16,16 +16,67 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
 using System.Threading.Tasks;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
 
 namespace JAMTech
 { 
     public class Startup
     {
         public const string ApiTitle = "JAM Tech Public API";
+        internal static Dictionary<string, Tuple<string, byte[]>> staticFilesStorage = new Dictionary<string, Tuple<string, byte[]>>();
+        internal static bool cache = true;
+        internal static string basePath = Path.GetFullPath("wwwroot");
 
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
+            LoadCache();
+        }
+
+        private static void LoadCache()
+        {
+            //put static files minificated in a memory store -> cache        
+            if (cache)
+            {
+                //read all static files and load into mmemory
+                var files = Directory.GetFiles(basePath, "*", SearchOption.AllDirectories);
+                var minifyJs = new WebMarkupMin.Core.CrockfordJsMinifier();
+                var minifyCss = new WebMarkupMin.Core.KristensenCssMinifier();
+                var minifyHtml = new WebMarkupMin.Core.HtmlMinifier();
+                foreach (var file in files)
+                {
+                    //TODO compress or minify
+                    var content = File.ReadAllBytes(file);
+                    var ext = Path.GetExtension(file);
+                    switch (ext)
+                    {
+                        case ".js":
+                            //javascript minify
+                            var js = System.Text.UTF8Encoding.Default.GetString(content);
+                            var minifiedJs = minifyJs.Minify(js, false);
+                            if (!minifiedJs.Errors.Any())
+                                content = System.Text.UTF8Encoding.Default.GetBytes(minifiedJs.MinifiedContent);
+                            break;
+                        case ".css":
+                            //css minify
+                            var css = System.Text.UTF8Encoding.Default.GetString(content);
+                            var minifiedCss = minifyCss.Minify(css, false);
+                            if (!minifiedCss.Errors.Any())
+                                content = System.Text.UTF8Encoding.Default.GetBytes(minifiedCss.MinifiedContent);
+                            break;
+                        case ".html":
+                            //html minify
+                            var html = System.Text.UTF8Encoding.Default.GetString(content);
+                            var minifiedHtml = minifyHtml.Minify(html, false);
+                            if (!minifiedHtml.Errors.Any())
+                                content = System.Text.UTF8Encoding.Default.GetBytes(minifiedHtml.MinifiedContent);
+                            break;
+                    }
+                    staticFilesStorage.Add(file, new Tuple<string, byte[]>(MimeMapping.MimeUtility.GetMimeMapping(file), content));
+                }
+                Console.WriteLine("Static files loaded to memory!!");
+            }
         }
 
         public IConfiguration Configuration { get; }
@@ -93,6 +144,8 @@ namespace JAMTech
 
             //app.UseRequestLocalization(BuildLocalizationOptions());
 
+
+
             // Middleware            
             app.Use(async (context, nextMiddleware) =>
             {
@@ -106,29 +159,29 @@ namespace JAMTech
                     }
                     context.Response.Headers.Add("X-Robots-Tag", "noindex");
                     context.Response.Headers.Add("Access-Control-Allow-Origin", "*");
+
                     return Task.FromResult(0);
                 });
                 await nextMiddleware();
             });
 
-
             app.UseDefaultFiles();
-            app.UseStaticFiles(new StaticFileOptions
-            {
-                OnPrepareResponse = ctx =>
-                {
-                    const int durationInSeconds = 0; // 60 * 60 * 24;
-                    if (durationInSeconds == 0)
-                    {
-                        ctx.Context.Response.Headers[HeaderNames.CacheControl] = "no-cache, no-store, must-revalidate";
-                    }
-                    //else
-                    //    ctx.Context.Response.Headers[HeaderNames.CacheControl] = "public,max-age=" + durationInSeconds;
-                }
-            });
+            //app.UseStaticFiles(new StaticFileOptions
+            //{
+            //    OnPrepareResponse = ctx =>
+            //    {
+            //        const int durationInSeconds = 0; // 60 * 60 * 24;
+            //        if (durationInSeconds == 0)
+            //        {
+            //            ctx.Context.Response.Headers[HeaderNames.CacheControl] = "no-cache, no-store, must-revalidate";
+            //        }
+            //        //else
+            //        //    ctx.Context.Response.Headers[HeaderNames.CacheControl] = "public,max-age=" + durationInSeconds;
+            //    }
+            //});
 
             app.UseMvc();
-            app.UseSwagger(c=>
+            app.UseSwagger(c =>
             {
                 c.PreSerializeFilters.Add((swaggerDoc, httpReq) => swaggerDoc.Host = httpReq.Host.Value);
             });
@@ -137,7 +190,30 @@ namespace JAMTech
                 c.DocumentTitle = ApiTitle;
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", ApiTitle);
             });
-        }
+
+            //Now let's add our custom middleware
+            //app.UseMiddleware<CacheMiddleware>();
+
+            // Custom middleware to get static files from cache           
+            app.Use(async (context, nextMiddleware) =>
+            {
+                if (Startup.cache && context.Request.Method == "GET" && Path.GetExtension(context.Request.Path) != string.Empty)
+                {
+                    //get file from cache and put into response
+                    var filename = Startup.basePath + context.Request.Path.Value.Replace("/", Path.DirectorySeparatorChar.ToString());
+                    if (Startup.staticFilesStorage.TryGetValue(filename, out var result))
+                    {
+                        var originalStream = context.Response.Body;
+                        context.Response.StatusCode = 200;
+                        context.Response.ContentType = result.Item1;
+                        context.Response.ContentLength = result.Item2.Length;
+                        await originalStream.WriteAsync(result.Item2, 0, result.Item2.Length);
+                    }
+                    else
+                        context.Response.StatusCode = 404;
+                }
+            });
+        }            
 
         private string GetXmlCommentsPath()
         {
