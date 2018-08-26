6ea87ee74309255138abae8e33ec82900ad65930
Jorge Aguado Merlet <aguado.jorge@principal.com>
2018-08-26 16:26:32 -0300

    change init page

--- wwwroot/js/config.js
+++ wwwroot/js/config.js
@@ -7,7 +7,7 @@
  *
  */
 function config($stateProvider, $urlRouterProvider, $ocLazyLoadProvider) {
-    $urlRouterProvider.otherwise("/index/torrents");
+    $urlRouterProvider.otherwise("/index/combustible");
 
     $ocLazyLoadProvider.config({
         // Set to true if you want to see what and when is dynamically loaded
