f794890ade9aa4c4fa735114d6a14a8f01e528c3
Jorge Aguado Merlet <aguado.jorge@principal.com>
2018-09-14 01:35:08 -0300

    add mercado libre to products search

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -1,4 +1,5 @@
-﻿using JAMTech.Helpers;
+﻿using HtmlAgilityPack;
+using JAMTech.Helpers;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
@@ -25,8 +26,11 @@ namespace JAMTech.Controllers
             };
             var knastaTasks =  Enumerable.Range(1, pages)
                                .Select(page => FindProductsKnastaAsync(product, page));
+            var mercadoClTasks = Enumerable.Range(1, pages)
+                               .Select(page => FindProductsMercadoLibreClAsync(product, page));
 
             tasks.AddRange(knastaTasks);
+            tasks.AddRange(mercadoClTasks);
             await Task.WhenAll(tasks);
             var flattenResult = tasks.Where(t => t.IsCompletedSuccessfully && t.Result as OkObjectResult != null)
                                 .Select(s => s.Result as OkObjectResult)
@@ -61,6 +65,41 @@ namespace JAMTech.Controllers
                         source = "knasta"
                     });
                     return new OkObjectResult(commonFormat);
+                }
+            }
+            return new NotFoundResult();
+        }
+
+
+        const string urlMercadoCl = "https://listado.mercadolibre.cl/{0}";
+        private static async Task<IActionResult> FindProductsMercadoLibreClAsync(string product, int page = 1)
+        {
+            using (var response = await Net.GetResponse(string.Format(urlMercadoCl, product)))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    var content = await response.Content.ReadAsStringAsync();
+                    //Parse results
+                    var results = new List<Models.Product>();
+                    var htmlDoc = new HtmlDocument();
+                    htmlDoc.LoadHtml(content);
+
+                    var resultsTable = htmlDoc.DocumentNode.Descendants().Where
+                            (x => (x.Name == "li" && x.Attributes["class"] != null &&
+                               x.Attributes["class"].Value.Contains("results-item"))).ToList();
+                    resultsTable.ForEach(r =>
+                    {
+                        //TODO include validations
+                        var imgUrl = r.Descendants("img").First().Attributes.Contains("src") ? r.Descendants("img").First().Attributes["src"].Value : r.Descendants("img").First().Attributes["data-src"].Value;
+                        results.Add(new Models.Product
+                        {
+                            description= r.Descendants("span").First(f => f.Attributes["class"] != null && f.Attributes["class"].Value.Contains("main-title")).InnerHtml,
+                            price= new List<Models.Price> { new Models.Price { price=int.Parse(r.Descendants("span").First(f => f.Attributes["class"] != null && f.Attributes["class"].Value.Contains("price__fraction")).InnerHtml.Replace(".","")) } },
+                            thumb= imgUrl,
+                            product_type= r.Descendants("a").First(f => f.Attributes["class"] != null && f.Attributes["class"].Value.Contains("item__info-title")).Attributes["href"].Value
+                        });
+                    });
+                    return new OkObjectResult(results);
                 }
             }
             return new NotFoundResult();
