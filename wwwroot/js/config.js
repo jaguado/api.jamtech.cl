698cd35ce3aa89dcfcd40750865a26e24c9efabe
Jorge Aguado Merlet <aguado.jorge@principal.com>
2018-09-16 22:42:38 -0300

    disable ocLazyLoadProvider debug

--- wwwroot/js/config.js
+++ wwwroot/js/config.js
@@ -46,7 +46,7 @@ function config($stateProvider, $urlRouterProvider, $ocLazyLoadProvider) {
 
     $ocLazyLoadProvider.config({
         // Set to true if you want to see what and when is dynamically loaded
-        debug: true
+        debug: false
     });
 
     $stateProvider
