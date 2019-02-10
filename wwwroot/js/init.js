f3fade001fc3f27d42478505ae3a60bc72d8bf83
Jorge Aguado Merlet <aguado.jorge@principal.com>
2019-02-10 17:59:38 -0300

    disable redirection

--- wwwroot/js/init.js
+++ wwwroot/js/init.js
@@ -2,7 +2,8 @@ if (document.location.hostname !== "localhost" &&
     document.location.hostname !== "l10pc0jruck.principalusa.corp.principal.com" &&
     document.location.hostname !== "l10pc0jruck.pvchiscl.principal.com" &&
     (document.location.hostname !== "aio.jamtech.cl" || document.location.protocol !== "https:")) {
-    document.location = "https://aio.jamtech.cl" + document.location.pathname;
+    //document.location = "https://aio.jamtech.cl" + document.location.pathname;
+    console.log('redirect ommited');
 } else
     console.log('redirect skipped for hostname: ' + document.location.hostname);
 
