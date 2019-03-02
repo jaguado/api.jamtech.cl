69bab3e9f1424552842dbb50625aebf87febb202
Jorge <jaguadom@gmail.com>
2019-03-02 01:23:50 -0300

    Fix (#31)
    * refactor
    
    * add remember monitoring
    
    * add newrelic
    
    * fix remember scheduler and mongodb update
    
    * add auto controller to get models and prices
    
    * add lastest newrelic agent
    
    * FIX pending changes

--- Helpers/Remember.cs
+++ Helpers/Remember.cs
@@ -10,7 +10,7 @@ namespace JAMTech.Helpers
 {
     public class Remember : IDisposable
     {
-        public Timer Timer { get; set; }
+        public List<Timer> Timers { get; set; } = new List<Timer>();
         public string Uid { get; set; }
         public Models.RememberConfig Config { get; set; }
         public Thread _rememberThread = null;
@@ -43,58 +43,77 @@ namespace JAMTech.Helpers
         {
             Console.WriteLine($"Starting '{Config.Name}' remember task at {Config.StartDate}");
             //create task
-            var intervalInMillis = 0;
-            switch (Config.RecurrenceType)
-            {
-                case Models.RememberConfig.RecurrenceTypes.Minutes:
-                    intervalInMillis = Config.RecurrenceValue * 60 * 1000;
-                    break;
-                case Models.RememberConfig.RecurrenceTypes.Hours:
-                    intervalInMillis = Config.RecurrenceValue * 60 * 60 * 1000;
-                    break;
-                case Models.RememberConfig.RecurrenceTypes.Days:
-                    intervalInMillis = Config.RecurrenceValue * 60 * 60 * 24 * 1000;
-                    break;
-                case Models.RememberConfig.RecurrenceTypes.Weeks:
-                    intervalInMillis = Config.RecurrenceValue * 60 * 60 * 24 * 7 * 1000;
-                    break;
-                case Models.RememberConfig.RecurrenceTypes.Months:
-                    intervalInMillis = Config.RecurrenceValue * 60 * 60 * 24 * 7 * 30 * 1000;
-                    break;
-                case Models.RememberConfig.RecurrenceTypes.Years:
-                    intervalInMillis = Config.RecurrenceValue * 60 * 60 * 24 * 7 * 364 * 1000;
-                    break;
-            }
-            Timer = ScheduleTask(Config.StartDate, Config.LastRun, intervalInMillis, async ()=>
-            {
-                Console.WriteLine($"Time to remember '{Config.Name}' timer !!");
-                //execute action -> Mail, SMS, Push
-                switch (Config.Type)
+            if(Config.RememberEarlier!=null)
+                Config.RememberEarlier.ForEach(earlier =>
                 {
-                    case Models.RememberConfig.RememberType.Email:
-                        if (await RememberByEmail(Config))
-                        {
-                            Config.LastRun = DateTime.Now;
-                            await UpdateConfig();
-                        }
-                        break;
-                    default:
-                        throw new NotImplementedException($"Remember action not implemented for type '{Config.Type.ToString()}'");
-                }
-                
-            });
+                    var milis = GetIntervalInMillis(earlier.Item1, earlier.Item2);
+                    Timers.Add(ScheduleTask(Config.StartDate.AddMilliseconds(milis * -1), null, 0, () => RememberNow(false)));
+                });
+            var intervalInMillis = GetIntervalInMillis(Config.RecurrenceType, Config.RecurrenceValue);
+            Timers.Add(ScheduleTask(Config.StartDate, Config.LastRun, intervalInMillis, () => RememberNow()));
             Thread.Sleep(Timeout.Infinite);//wait forever
         }
 
-        private static async Task<bool> RememberByEmail(Models.RememberConfig config)
+        private static int GetIntervalInMillis(Models.RememberConfig.RecurrenceTypes recurrenceType, int value)
+        {
+            var intervalInMillis = 0;
+            switch (recurrenceType)
+            {
+                case Models.RememberConfig.RecurrenceTypes.Minutes:
+                    intervalInMillis = value * 60 * 1000;
+                    break;
+                case Models.RememberConfig.RecurrenceTypes.Hours:
+                    intervalInMillis = value * 60 * 60 * 1000;
+                    break;
+                case Models.RememberConfig.RecurrenceTypes.Days:
+                    intervalInMillis = value * 60 * 60 * 24 * 1000;
+                    break;
+                case Models.RememberConfig.RecurrenceTypes.Weeks:
+                    intervalInMillis = value * 60 * 60 * 24 * 7 * 1000;
+                    break;
+                case Models.RememberConfig.RecurrenceTypes.Months:
+                    intervalInMillis = value * 60 * 60 * 24 * 7 * 30 * 1000;
+                    break;
+                case Models.RememberConfig.RecurrenceTypes.Years:
+                    intervalInMillis = value * 60 * 60 * 24 * 7 * 364 * 1000;
+                    break;
+            }
+            return intervalInMillis;
+        }
+
+        private async void RememberNow(bool isNow = true)
+        {
+            if(isNow)
+                Console.WriteLine($"Time to remember '{Config.Name}' timer !!");
+            //execute action -> Mail, SMS, Push
+            switch (Config.Type)
+            {
+                case Models.RememberConfig.RememberType.Email:
+                    if (await RememberByEmail(Config, isNow))
+                    {
+                        Config.LastRun = DateTime.Now;
+                        await UpdateConfig();
+                    }
+                    break;
+                default:
+                    throw new NotImplementedException($"Remember action not implemented for type '{Config.Type.ToString()}'");
+            }
+        }
+
+        static bool disableSendEmail = Environment.GetEnvironmentVariable("disableSendEmail", EnvironmentVariableTarget.Process).Trim().ToLower().Equals("true");
+        private static async Task<bool> RememberByEmail(Models.RememberConfig config, bool isNow)
         {
             var subject = $"Hi, this mail is a remembering for '{config.Name}'";
             var to = config.RememberTo.Select(mail => new SendGrid.Helpers.Mail.EmailAddress(mail));
-            var response = await Email.SendLegacy(Email.defaultFrom, to.ToList(), subject, config.TemplateId, config.Substitutions);
-            if (response.StatusCode != System.Net.HttpStatusCode.Accepted)
+            
+            if (!disableSendEmail)
             {
-                Console.WriteLine($"Error ocurred when tried to remember '{config.Name}'");
-                return false;
+                var response = await Email.SendLegacy(Email.defaultFrom, to.ToList(), subject, config.TemplateId, config.Substitutions);
+                if (response.StatusCode != System.Net.HttpStatusCode.Accepted)
+                {
+                    Console.WriteLine($"Error ocurred when tried to remember '{config.Name}'");
+                    return false;
+                }
             }
             return true;
         }
@@ -105,11 +124,8 @@ namespace JAMTech.Helpers
         }
         public Timer ScheduleTask(DateTime initialDate, DateTime? lastRun, int intervalInMilis, Action task)
         {
-            var runAt = lastRun.HasValue ? lastRun.Value : initialDate;
-            var dueTime = (runAt + TimeSpan.FromMilliseconds(intervalInMilis)) - DateTime.Now;
-            if (dueTime.TotalMilliseconds < 0)
-                dueTime = TimeSpan.FromSeconds(0);
-
+            var dueTime = (lastRun ?? initialDate + TimeSpan.FromMilliseconds(intervalInMilis)) - DateTime.Now;
+            if (dueTime.TotalMilliseconds < 0) dueTime = TimeSpan.FromSeconds(0);
             return new Timer(_ => task.Invoke(), null, dueTime, TimeSpan.FromMilliseconds(intervalInMilis));
         }
     }
