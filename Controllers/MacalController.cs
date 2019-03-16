ade0cf2a6b93e41a33e39540586d2bbd7d4d6eaa
Jorge <jaguadom@gmail.com>
2019-03-16 20:55:52 -0300

    Add Macal v1 (#33)
    * refactor
    
    * add remember monitoring
    
    * add newrelic
    
    * fix remember scheduler and mongodb update
    
    * add auto controller to get models and prices
    
    * add lastest newrelic agent
    
    * FIX pending changes
    
    * allow anony access to autos controller
    
    * remove newrelic, mongodb and sendgrid apikey
    
    * add macal endpoints

---
+++ Controllers/MacalController.cs
@@ -0,0 +1,62 @@
+﻿using JAMTech.Helpers;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using JAMTech.Extensions;
+using System.Threading;
+using JAMTech.Filters;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+using Microsoft.AspNetCore.Authorization;
+using System.IO;
+using System.Net.Http.Formatting;
+using System.Text;
+
+namespace JAMTech.Controllers
+{
+    [Route("v1/[controller]")]
+    public class MacalController : BaseController
+    {
+        private static Models.Macal _macal = null;
+
+        [AllowAnonymous]
+        [HttpGet()]
+        public async Task<IActionResult> GetVehicles(int idRemate = 422)
+        {
+            if (_macal == null)
+            {
+                const string url = @"https://www.macal.cl/Vehiculos/VehiculosBienesGet";
+                var macalBody = await new HttpClient().PostAsync(url, new StringContent("{'carrusel':'NO','id_remate':'" + idRemate + "'}",  Encoding.UTF8, "application/json"));
+                _macal = await macalBody.Content.ReadAsAsync<Models.Macal>();
+            }
+            return new OkObjectResult(_macal);
+        }
+        [AllowAnonymous]
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetVehicle(int id)
+        {
+            if (_macal == null)
+            {
+                await GetVehicles();
+            }
+            var vehicle = _macal.Bienes.First(b => b.Bienid == id);
+            if(vehicle.Detalle == null)
+            {
+                //TODO complete detail
+                const string url = @"https://www.macal.cl/Detalle/Vehiculo/";
+                var detailBody = await new HttpClient().GetStringAsync(url + id.ToString());
+                const string tag = "dataLayer =";
+                var startIndex = detailBody.IndexOf(tag) + 1;
+                var endIndex = detailBody.IndexOf("}];", startIndex);
+                var data = detailBody.Substring(startIndex + tag.Length, endIndex - (startIndex + tag.Length) + 2);
+                vehicle.Detalle = JsonConvert.DeserializeObject<object[]>(data)[0];
+            }
+            return new OkObjectResult(vehicle);
+        }
+
+    }
+}
---
+++ Models/Macal.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JAMTech.Models
+{
+    public class CuotaTanner
+    {
+        public double ValorUf { get; set; }
+        public string NombreCliente { get; set; }
+        public string ModeloVehiculo { get; set; }
+        public string PPU { get; set; }
+        public int ValorVehiculo { get; set; }
+        public int Pie { get; set; }
+        public int Plazo { get; set; }
+        public int SaldoPrecio { get; set; }
+        public int ComisionOtorgada { get; set; }
+        public int GastosNotariales { get; set; }
+        public int GastosPrenda { get; set; }
+        public double MontoFinanciar { get; set; }
+        public double SeguroDesgravamen { get; set; }
+        public int Impuestos { get; set; }
+        public double TasaInteres { get; set; }
+        public double ValorCuota { get; set; }
+        public double CAE { get; set; }
+        public int CostoTotalCredito { get; set; }
+        public int CostoTotalVehiculo { get; set; }
+        public bool Destacado { get; set; }
+        public string LegalConValores { get; set; }
+        public string LegalTanner { get; set; }
+        public string LegalConValoresConPie { get; set; }
+        public string LegalConValoresConPieSinRentayModeloVehiculo { get; set; }
+        public string LegalConValoresSinPie { get; set; }
+    }
+
+    public class Biene
+    {
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public string __invalid_name__Año { get; set; }
+        public object Docbienurl { get; set; }
+        public object Url_img { get; set; }
+        public double Precio { get; set; }
+        public object CajaTransmision { get; set; }
+        public object Traccion { get; set; }
+        public object Combustible { get; set; }
+        public object Kilometraje { get; set; }
+        public object PlacaUnica { get; set; }
+        public object Exhibicion { get; set; }
+        public bool Destacado { get; set; }
+        public object Certificado_anotaciones { get; set; }
+        public object Garantia_mecanica { get; set; }
+        public double Garantia { get; set; }
+        public CuotaTanner CuotaTanner { get; set; }
+        public object IdRemate { get; set; }
+        public int Bienid { get; set; }
+        public object Nombre { get; set; }
+        public object Descripcion { get; set; }
+        public List<string> Imagenes { get; set; }
+        public int Tipremid { get; set; }
+        public int Remid { get; set; }
+        public object Biendescripcionweb { get; set; }
+        public int Docbienid { get; set; }
+        public object Esdestacado { get; set; }
+        public string Loteid { get; set; }
+        public int LOTECODIGOCONTADOR { get; set; }
+        public int RemCodigoContador { get; set; }
+        public int Monid { get; set; }
+        public object Detloteminimo { get; set; }
+        public object Categoria { get; set; }
+        public int LoteoDefinitivo { get; set; }
+        public int NumeroLote { get; set; }
+        public dynamic Detalle { get; set; }
+    }
+
+    public class Macal
+    {
+        public DateTime FechaRemate { get; set; }
+        public object IdRemate { get; set; }
+        public List<Biene> Bienes { get; set; }
+    }
+}
