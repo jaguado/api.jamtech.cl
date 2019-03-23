87e9bd2be3dcf6bf5136df7e3e59a253b70792c4
Jorge <jaguadom@gmail.com>
2019-03-23 01:42:56 -0300

    Macal fix (#37)
    * refactor
    
    * add remember monitoring
    
    * add newrelic
    
    * fix remember scheduler and mongodb update
    
    * add auto controller to get models and prices
    
    * add lastest newrelic agent
    
    * FIX pending changes
    
    * allow anony access to autos controller
    
    * remove newrelic, mongodb and sendgrid apikey
    
    * add macal endpoints
    
    * add macal search
    
    * refactor
    
    * fix
    
    * refactor
    
    * add custom info
    
    * add custom prices and other info
    
    * fix swagger

---
+++ Helpers/MacalCalculations.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JAMTech.Helpers
+{
+    public static class MacalCalculations
+    {
+        public static double GetRealPrice(double price, double fiscalValue = 0)
+        {
+            if (fiscalValue < price)
+                return price + GetComission(price) + (price * 0.015) + 75000;
+            else
+            {
+                return price + GetComission(price) + (fiscalValue * 0.015) + 75000;
+            }
+        }
+
+        public static double GetPrice(double realPrice, double fiscalValue = 0)
+        {
+            if (fiscalValue < realPrice)
+                return realPrice - GetComission(realPrice) - (realPrice * 0.015) - 75000;
+            else
+            {
+                return realPrice - GetComission(realPrice) - (fiscalValue * 0.015) - 75000;
+            }
+        }
+
+        private static double GetComission(double price)
+        {
+            return price * 0.125;
+        }
+    }
+}
